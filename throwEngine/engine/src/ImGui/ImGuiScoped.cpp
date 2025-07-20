9ba37b718329d9f69d706047f41461ac5c505d74
pointerlost <zlatanovic625@gmail.com>
2025-07-20 15:58:46 +0300

    some stuff

--- throwEngine/engine/src/ImGui/ImGuiScoped.cpp
+++ throwEngine/engine/src/ImGui/ImGuiScoped.cpp
@@ -1,8 +1,7 @@
 #include "ImGui/ImGuiScoped.h"
 
-#include <imgui/imgui_impl_glfw.h>
-
-#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
 
 #include "core/Logger.h"
 #include "core/Debug.h"
